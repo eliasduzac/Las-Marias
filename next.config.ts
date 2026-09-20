740c3d13a172db3a19d6cdb8b0bc242cf1f20ffe
GMCargoSRL <gmcargosrl@gmail.com>
2026-09-19 22:51:11 -0300

    Actualización general

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
-  disable: process.env.NODE_ENV === "development", // Se desactiva en local para que no moleste
+  disable: process.env.NODE_ENV === "development",
 });
 
 export default withPWA({
-  // Acá podés dejar otras configuraciones de Next si ya tenías
+  turbopack: {},
 });
